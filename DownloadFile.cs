0f71b1b2873c1bdbe765e8e62811e39386c508d0
LiliiaKlymenko <liliia.klymenko@hneu.net>
2015-10-13 14:09:26 +0300

    project created
    project created

---
+++ DownloadFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace TestProject
+{
+	class DownloadFile
+	{
+		public static void downloadFileFromURL(string address, string fileName)
+		{
+			using (WebClient webClient = new WebClient())
+			{
+				Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n", fileName, address);
+				webClient.DownloadFile(address, @fileName);
+				Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, address);
+				string appDirectory = Environment.CurrentDirectory;
+				Console.WriteLine("\nThe file is saved at:\t" + fileName);
+				Console.ReadLine();
+			}
+
+		}
+	}
+}
---
+++ DownloadFileTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace TestProject
+{
+	[TestClass]
+	public class DownloadFileTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			#region Test data
+			string address = "https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt";
+			string fileName = @"c:\NewTxt.txt";
+			#endregion
+
+			DownloadFile.downloadFileFromURL(address, fileName);
+			Assert.IsTrue(File.Exists(fileName));
+		}
+	}
+}
